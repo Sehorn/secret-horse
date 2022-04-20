0000d6680556182a8bd3d6b1b04287a569f99cf0
Austin Sehorn <Austinsehorn@gmail.com>
2022-04-20 10:05:38 -0700

    added background gif to local folder for smoother scrolling

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,7 @@ module.exports = {
         "half-screen": "50vh",
       },
       backgroundImage: {
-        "pack-train":
-          "url('https://64.media.tumblr.com/a86b91712abc92e44fdf1d5162c0938a/678b5d737f8219ed-28/s640x960/79628bb9d5e4456fac22b0f0f9a009f456e4b8e9.gif')",
+        "pack-train": "url('/background.gif')",
       },
     },
   },
